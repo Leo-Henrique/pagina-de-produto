4271856c68c284d5efe7c797e2b98dd24f4e92a4
Leonardo Henrique <leonardo0507.henrique@gmail.com>
2022-06-15 18:28:36 -0300

    Clique fora melhorado

--- js/modules/click-outside.js
+++ js/modules/click-outside.js
@@ -1,78 +1,27 @@
 export default function clickOutside(elements, callback) {
     const attr = "data-clickOutside";
 
-    if (elements.length === 1) {
-        if (!elements[0].hasAttribute(attr)) {
-            elements[0].setAttribute(attr, "");
-            document.addEventListener("click", handleOutsideClick);
-        }
-    
-        function handleOutsideClick(e) {
-            if (!elements[0].contains(e.target)) {
-                callback();
-                elements[0].removeAttribute(attr);
-                document.removeEventListener("click", handleOutsideClick);
-            }
-        }
-
-    } else {
+    function handleAttribute() {
         elements.forEach((element) => {
+            const children = element.querySelectorAll("*");
+
             if (!element.hasAttribute(attr)) {
                 element.setAttribute(attr, "");
-                element.querySelectorAll("*").forEach((children) => children.setAttribute(attr, ""));
+                children.forEach((child) => child.setAttribute(attr, ""));
+            } else {
+                element.removeAttribute(attr, "");
+                children.forEach((child) => child.removeAttribute(attr, ""));
             }
-        });    
-
-        function handleOutsideClick(e) {
-            if (!e.target.hasAttribute(attr)) {
-                callback();
-                document.removeEventListener("click", handleOutsideClick);
-
-                elements.forEach((element) => {
-                    element.removeAttribute(attr, "");
-                    element.querySelectorAll("*").forEach((children) => children.removeAttribute(attr, ""));
-                });    
-            }
-        }
-        document.addEventListener("click", handleOutsideClick);
+        });
     }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-    if (!element.hasAttribute(attr)) {
-        element.setAttribute(attr, "");
-        document.addEventListener("click", handleOutsideClick);
-    }
+    handleAttribute();
 
     function handleOutsideClick(e) {
-        if (!element.contains(e.target)) {
+        if (!e.target.hasAttribute(attr)) {
             callback();
-            element.removeAttribute(attr);
+            handleAttribute();
             document.removeEventListener("click", handleOutsideClick);
         }
     }
-
-*/
+    document.addEventListener("click", handleOutsideClick);
+}
